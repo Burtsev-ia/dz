79c5755cab2e918502f9a4a595688285c10cc188
Burtsev-ia <trinta2007@gmail.com>
2025-11-11 00:16:17 +0300

    практически

--- sem9/dz9.5.py
+++ sem9/dz9.5.py
@@ -1,10 +1,16 @@
 import heapq
 a = list(map(int, input().split()))
 heapq.heapify(a)
+
+
 def heapsort(x):
-    l = len(x)
-    for i in range(l):
-        x[0], x[l-1-i] = x[l-1-i], x[0]
-        heapq._siftdown(a, 0, 1)
+    p = len(x)
+    for i in range(p):
+        # print('1')
+        x[0], x[p-1-i] = x[p-1-i], x[0]
+        heapq._siftup(x, 0)
+        # print(x)
+    return x
+
+
 print(heapsort(a))
-        
